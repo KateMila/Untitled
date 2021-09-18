97de63d05e877e167ec89217a8dc047b794e5ccb
KateMila <89808903+KateMila@users.noreply.github.com>
2021-09-18 22:45:11 +0300

    2 commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int balance = 2000000000;
+        int remittance = 500000000;
+        int account_balance = balance - remittance;
+        System.out.println(account_balance);
+    }
+}
